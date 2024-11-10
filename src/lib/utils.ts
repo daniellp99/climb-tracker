fcd65d16d3f370604165929c1e8b83b377c356f8
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-11-10 01:04:16 -0500

    feat(DB): add generic action state type for work with server actions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { type z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,3 +10,10 @@ export function generateInitials(name: string) {
   const [firstName, lastName] = name.split(" ", 2);
   return String(firstName?.slice(0, 1)) + String(lastName?.slice(0, 1));
 }
+
+export type ActionState<TSchemaType extends z.ZodType> = {
+  status: "success" | "error" | "initial";
+  message: string;
+  errors?: z.inferFlattenedErrors<TSchemaType>["fieldErrors"];
+  payload?: Partial<z.infer<TSchemaType>>;
+};
